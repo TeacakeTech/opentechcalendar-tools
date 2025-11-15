80613e8861cf12f0d26c9a002f991c88848a3f75
James <james@jarofgreen.co.uk>
2025-11-15 19:37:33 +0000

    Ical import work
    * More fields
    * New line at end for neater data files

--- opentechcalendartools/worker.py
+++ opentechcalendartools/worker.py
@@ -29,10 +29,8 @@ class Worker:
             group = res.fetchone()
             if group["field_import_type"] == "ical":
                 self._import_group_type_ical(
-                    group_id,
+                    group,
                     self._download_file_to_temp(group["field_import_url"]),
-                    group_country=group["field_country"],
-                    group_place=group["field_place"],
                 )
 
     def _download_file_to_temp(self, url) -> str:
@@ -46,9 +44,9 @@ class Worker:
         return new_filename_dets[1]
 
     def _import_group_type_ical(
-        self, group_id, ical_filename, group_country=None, group_place=None
+        self, group, ical_filename, group_country=None, group_place=None
     ):
-        os.makedirs(os.path.join(self._data_dir, "event", group_id), exist_ok=True)
+        os.makedirs(os.path.join(self._data_dir, "event", group["id"]), exist_ok=True)
         with open(ical_filename) as fp:
             calendar = icalendar.Calendar.from_ical(fp.read())
             for event in calendar.events:
@@ -57,23 +55,33 @@ class Worker:
                 if end.dt.timestamp() > datetime.datetime.now().timestamp():
                     event_data = {
                         "title": str(event.get("SUMMARY")),
-                        "group": group_id,
+                        "group": group["id"],
+                        "timezone": group["field_timezone"] or "UTC",
                         "start_at": str(start.dt),
                         "end_at": str(end.dt),
                         "url": str(event.get("URL")),
                         "cancelled": (event.get("STATUS") == "CANCELLED"),
                         "imported": True,
                     }
-                    if group_country:
-                        event_data["country"] = group_country
-                    if group_place:
-                        event_data["place"] = group_place
+                    if group["field_country"]:
+                        event_data["country"] = group["field_country"]
+                    if group["field_place"]:
+                        event_data["place"] = group["field_place"]
+                    if group["field_code_of_conduct_url"]:
+                        event_data["code_of_conduct_url"] = group[
+                            "field_code_of_conduct_url"
+                        ]
+                    if group["field_in_person"] == "always":
+                        event_data["in_person"] = "yes"
+                    elif group["field_in_person"] == "never":
+                        event_data["in_person"] = "no"
                     id = event.get("UID").split("@").pop(0)
                     filename = os.path.join(
-                        self._data_dir, "event", group_id, id + ".md"
+                        self._data_dir, "event", group["id"], id + ".md"
                     )
                     with open(filename, "w") as fp:
                         fp.write("---\n")
                         fp.write(yaml.dump(event_data))
                         fp.write("---\n\n\n")
                         fp.write(event.get("DESCRIPTION"))
+                        fp.write("\n")
